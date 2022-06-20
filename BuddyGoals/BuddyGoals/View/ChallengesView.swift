e8f21fc4236257e5135d6bd3669dac57a81ded4b
gusde27 <gusdechang24@gmail.com>
2022-06-20 23:48:15 +0800

    Make a MVVM and Tabbar

--- BuddyGoals/BuddyGoals/View/ChallengesView.swift
+++ BuddyGoals/BuddyGoals/View/ChallengesView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+struct ChallengesView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
@@ -40,6 +40,7 @@ struct ContentView: View {
             }
             Text("Select an item")
         }
+        
     }
 
     private func addItem() {
@@ -81,8 +82,8 @@ private let itemFormatter: DateFormatter = {
     return formatter
 }()
 
-struct ContentView_Previews: PreviewProvider {
+struct ChallengesView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ChallengesView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
---
+++ BuddyGoals/BuddyGoals/View/TravenView.swift
@@ -0,0 +1,20 @@
+//
+//  TravenView.swift
+//  BuddyGoals
+//
+//  Created by I Gede Bagus Wirawan on 20/06/22.
+//
+
+import SwiftUI
+
+struct TravenView: View {
+    var body: some View {
+        Text("Traven Lol")
+    }
+}
+
+struct TravenView_Previews: PreviewProvider {
+    static var previews: some View {
+        TravenView()
+    }
+}
