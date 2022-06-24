f3574d8cfc4585773f4a102479cbfd81a6495094
jokev106 <62888793+jokev106@users.noreply.github.com>
2022-06-24 15:18:12 +0700

    tt

---
+++ BuddyGoals /BuddyGoals/View/Component/ProfileCardView.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileCardView.swift
+//  BuddyGoals
+//
+//  Created by I Gede Bagus Wirawan on 21/06/22.
+//
+
+import SwiftUI
+
+struct ProfileCardView: View {
+    var body: some View {
+        
+        HStack{
+            
+        }
+        
+    }
+}
+
+struct ProfileCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileCardView()
+    }
+}
---
+++ BuddyGoals /BuddyGoals/View/ProgressView.swift
@@ -0,0 +1,22 @@
+//
+//  ProgressView.swift
+//  BuddyGoals
+//
+//  Created by I Gede Bagus Wirawan on 20/06/22.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    //@EnvironmentObject var progress: Lol
+    
+    var body: some View {
+        Text("Progress View")
+    }
+}
+
+struct ProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressView()
+    }
+}
