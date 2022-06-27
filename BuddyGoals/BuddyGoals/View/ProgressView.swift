d39728a69a047ab0d0fb24af4fd091fcb3eb329d
GTerrence <tpramono@student.ciputra.ac.id>
2022-06-27 15:44:31 +0700

    Fixed lost files

---
+++ BuddyGoals/BuddyGoals/View/ProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  ProgressView.swift
+//  BuddyGoals
+//
+//  Created by Terrence Pramono on 27/06/22.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressView()
+    }
+}
